0740bfa339dbd8cef452e8cd65052a59b2cefa21
AyoubA <ayoubamnour@1.102.124.10.in-addr.arpa>
2014-06-11 17:04:02 +0100

    INITIAL SITE UPLOAD -AYOUB

---
+++ contact.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title> TOOLS FAM, TOOLS</title>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252" />
+<link rel="stylesheet" type="text/css" href="style.css" />
+<!--[if IE 6]>
+<link rel="stylesheet" type="text/css" href="iecss.css" />
+<![endif]-->
+<script type="text/javascript" src="js/boxOver.js"></script>
+</head>
+<body>
+
+<div id="main_container">
+  <div id="header">
+
+  
+      
+    <div class="big_banner"> 
+	<a href="">
+	<!--<img src="images/banner728.jpg" alt="" border="0" /></a> </div>-->
+	<h1> TOOLS FAM </h1>
+    </div>
+
+    <div id="logo">
+	 <a href="">
+ 	 <img src="images/logo.png" alt="" border="0" width="182" height="85" /></a> </div>
+    </div>
+
+  <div id="main_content" ALIGN="CENTER">
+    <div id="menu_tab" ALIGN="CENTER">
+
+<table align="center">
+<tr>
+
+
+
+<td>
+        <li><a href="index.php" class="nav"> Home </a></li>
+        </td>
+
+
+<td>
+        <li><a href="" class="nav">Products</a></li>
+        </td>
+
+
+<td>
+        <li><a href="" class="nav">Shipping </a></li>
+        </td>
+
+
+<td>
+        <li><a href="contact.php" class="nav">Contact Us</a></li>
+</td>
+
+
+</tr>
+</table>
+    </div>
+    <!-- end of menu tab -->
+
+
+	<!-- SIDE LINKS (CATEGORIES )-->
+    <div class="crumb_navigation"> Navigation: <span class="current">Home</span> </div>
+    <div class="left_content">
+      <div class="title_box">Categories</div>
+      <ul class="left_menu">
+        <li class="odd"><a href="">Power Tools</a></li>
+        <li class="even"><a href="">Air Tools</a></li>
+        <li class="odd"><a href="">Hand Tools</a></li>
+        <li class="even"><a href="">Accessories</a></li>
+        <li class="odd"><a href="">Workwear</a></li>
+        <li class="even"><a href="">Spare Parts</a></li>
+        <li class="odd"><a href="">Power Tools</a></li>
+        <li class="even"><a href="">Air Tools</a></li>
+        <li class="odd"><a href="">Hand Tools</a></li>
+        <li class="even"><a href="">Accessories</a></li>
+        <li class="odd"><a href="">Workwear</a></li>
+        <li class="even"><a href="">Spare Parts</a></li>
+      </ul>
+<!-- END OF CATEGORIES -->
+
+
+
+	<!-- Special Products (BELOW CATEGORIES )-->
+      <div class="title_box">Special Products</div>
+      <div class="border_box">
+        <div class="product_title"><a href="">Makita 156 MX-VL</a></div>
+        <div class="product_img"><a href=""><img src="images/p1.jpg" alt="" border="0" /></a></div>
+        <div class="prod_price"><span class="reduce">350$</span> <span class="price">270$</span></div>
+      </div>
+	<!-- Special Products (BELOW CATEGORIES )-->
+	</div>
+   <!-- end of left content -->
+
+<!-- middle section (PAGE CONTENT)-->
+   
+    <div class="center_content">
+<h1> Contact Us </h1>
+<br/>
+<br/>
+<br/>
+<form name="form1" method="post" action="contactUs.php">
+<table border="0" cellpadding="0" cellspacing="0" width="98%">
+<tbody>
+<tr>
+<td>First Name:</td>
+<td><label>
+<input name="UserName" id="UserName" type="text"> </label></td>
+<td>&nbsp;</td>
+</tr>
+
+<tr>
+<td>
+<br/>
+</td>
+</tr>
+<tr>
+
+<td>Last Name:</td>
+<td><input name="RealName" id="RealName" type="text"></td>
+</tr>
+
+<tr>
+<td>
+<br/>
+</td>
+</tr>
+<tr>
+
+<tr>
+<td>Email:</td>
+<td><input name="Email" id="Email" type="text"></td>
+</tr>
+
+<tr>
+<td>
+<br/>
+</td>
+</tr>
+<tr>
+
+<tr>
+<td>Subject:</td>
+<td><input name="Subject" id="Subject" type="text"></td>
+</tr>
+
+<tr>
+<td>
+<br/>
+</td>
+</tr>
+<tr>
+
+<tr>
+<td>Message:</td>
+<td><textarea cols="45"
+rows="5"></textarea></td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td>&nbsp;</td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td><label> <input name="submit" id="button" value="Submit"
+type="submit"> </label> <label> <input name="button2" id="button2"
+value="Clear" type="reset"> </label></td>
+</tr>
+</tbody>
+</table>
+</form>
+
+    </div>
+
+   
+
+<!-- end of page content (MIDDLE CONTENT)-->
+
+
+
+<!-- START OF right -->
+    <div class="right_content">
+      <div class="title_box">Search</div>
+
+<!-- START OF SEARCH-->
+      <div class="border_box">
+        <input type="text" name="newsletter" class="newsletter_input" value="keyword"/>
+        <a href="" class="join">search</a> </div>
+<!-- end of search-->
+
+ 
+<!-- START OF SHOPPING CART-->
+	<div class="shopping_cart">
+        <div class="title_box">Shopping cart</div>
+        <div class="cart_details"> 3 items <br />
+          <span class="border_cart"></span> Total: <span class="price">350$</span> </div>
+        <div class="cart_icon"><a href=""><img src="images/shoppingcart.png" alt="" width="35" height="35" border="0" /></a></div>
+      </div>
+<!-- end of shopping cart-->
+
+<!-- START OF WHATS NEW-->
+      <div class="title_box">What's new</div>
+      <div class="border_box">
+        <div class="product_title"><a href="">Motorola 156 MX-VL</a></div>
+        <div class="product_img"><a href=""><img src="images/p2.jpg" alt="" border="0" /></a></div>
+        <div class="prod_price"><span class="reduce">350$</span> <span class="price">270$</span></div>
+      </div>
+<!-- end of whats new-->
+
+<!-- START OF MANUFACTURERS-->
+      <div class="title_box">Manufacturers</div>
+      <ul class="left_menu">
+        <li class="odd"><a href="">Bosch</a></li>
+        <li class="even"><a href="">Samsung</a></li>
+        <li class="odd"><a href="">Makita</a></li>
+        <li class="even"><a href="">LG</a></li>
+        <li class="odd"><a href="">Fujitsu Siemens</a></li>
+        <li class="even"><a href="">Motorola</a></li>
+        <li class="odd"><a href="">Phillips</a></li>
+        <li class="even"><a href="">Beko</a></li>
+      </ul>
+
+<!-- end of MANUFACTURERS-->
+
+
+
+    </div>
+    <!-- end of right content -->
+  </div>
+  <!-- end of main content -->
+
+
+<!-- START OF FOOTER-->
+  <div class="footer">
+    <div class="left_footer"> <img src="images/footer_logo.png" alt="" width="89" height="42"/> </div>
+    <div class="center_footer">  AJM SOLUTIONS <br />
+      <a href="http://csscreme.com"><img src="images/csscreme.jpg" alt="csscreme" title="csscreme" border="0" /></a><br />
+      <img src="images/payment.gif" alt="" /> </div>
+    <div class="right_footer"> <a href="">home</a> <a href="">about</a> <a href="">sitemap</a> <a href="">rss</a> <a href="">contact us</a> </div>
+  </div>
+<!-- end of footer-->
+</div>
+<!-- end of main_container -->
+
+<div align=center> AJM SOLUTIONS <a href=''></a></div></body>
+</html>
